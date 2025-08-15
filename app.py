c8c6ca511be4bda661e18206abc393e2ee89a8f0
Mustafa Zetawi <mustafa.m.zetawi@gmail.com>
2025-08-15 10:30:47 +0300

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, redirect, url_for, request
 app = Flask(__name__)
-app.secret_key = "change-me"  
+app.secret_key = "mustafazetawi"  
 def init_session():
     if "counter" not in session:
         session["counter"] = 0
